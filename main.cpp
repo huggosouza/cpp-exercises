197dfbdb4781a241bcc41dac0409d6f430adef94
Hugo Souza <hugoedson2019@gmail.com>
2022-08-22 08:36:58 -0300

    modules and headers

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "mymath.h"
+
+using namespace std;
+
+int main(){
+	cout << "Hello, world!\n";
+	
+	int n = 5;
+	
+	cout << "Fatorial de n: "<< fatorial(5);
+	
+	return 0;
+}
---
+++ mymath.h
@@ -0,0 +1,8 @@
+#ifndef _MYMATH_H_
+#define _MYMATH_H_
+
+int fatorial(int n);
+int area_quadrado(int lado);
+int area_retangulo(int lado, int base);
+
+#endif
